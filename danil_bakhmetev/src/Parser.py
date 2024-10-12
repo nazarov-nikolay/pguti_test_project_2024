6930212445d460f8d8ccb9079095e2a0e461095d
kukuvs <122643187+kukuvs@users.noreply.github.com>
2024-10-12 17:40:42 +0400

    Готово
    + логи +sqlite
    + Проверка на ошибки

--- danil_bakhmetev/src/Parser.py
+++ danil_bakhmetev/src/Parser.py
@@ -33,16 +33,11 @@ class Parser_Employee:
                 # Читаем файл построчно и добавляем
                 # каждую строку в список data
                 for row in reader:
-                    self.data.append(tuple(row.values()))
+                    self.data.append(dict(row))
         except IOError as e:
             # если возникла ошибка при чтении файла,
             # то поднимаем исключение IOError
             raise IOError(f"Ошибка при чтении {path}: {e}") from e
 
 
-# создаем объект класса
-s = Parser_Employee('src/Employee_Details.csv')
-
-# печатаем результат
-print(s.data)
 
--- danil_bakhmetev/src/SQLmeneg.py
+++ danil_bakhmetev/src/SQLmeneg.py
@@ -1,42 +1,77 @@
-import psycopg2
+import sqlite3
+import logging
+from Parser import Parser_Employee
 
-# Установите соответствующие значения для вашего случая
-dbname = 'postgres'
-user = 'postgres'
-password = '42695'
-host = 'localhost'
-port = '5432'
+logging.basicConfig(level=logging.INFO)
 
-conn = None
-cur = None
+class SQLmeneg:
+    def __init__(self, path):
+        self.path = path
+        self.conn = sqlite3.connect('employee.db')
+        self.cur = self.conn.cursor()
+        
+    def create_db(self):
+        """Creates a database with the following structure:
 
-try:
-    # Подключение к базе данных
-    conn = psycopg2.connect(
-        dbname=dbname,
-        user=user,
-        password=password,
-        host=host,
-        port=port
-    )
-
-    # Создание курсора для выполнения SQL-запросов
-    cur = conn.cursor()
-
-    # Выполнение простого SQL-запроса
-    cur.execute('SELECT version()')
-
-    # Получение результата запроса
-    db_version = cur.fetchone()
-
-    print(f'Connected to PostgreSQL version: {db_version}')
-
-except Exception as e:
-    print(f'Error connecting to PostgreSQL: {e}')
-
-finally:
-    # Закрытие курсора и соединения
-    if cur:
-        cur.close()
-    if conn:
-        conn.close()
+        E_ID = int,
+        E_NAME = varchar(30),
+        E_DESIGNATION = varchar(40),
+        E_ADDR = varchar(100),
+        E_BRANCH = varchar(15),
+        E_CONT_NO = int
+        """
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS EMPLOYEE (
+                E_ID INTEGER PRIMARY KEY,
+                E_NAME VARCHAR(30),
+                E_DESIGNATION VARCHAR(40),
+                E_ADDR VARCHAR(100),
+                E_BRANCH VARCHAR(15),
+                E_CONT_NO INTEGER
+            )
+        """)
+        self.conn.commit()
+        
+    def add_employee(self):
+        """добовление всего что содежиться в csv используя парсер"""
+        logging.info("Start adding employee")
+        s = Parser_Employee(self.path)
+       
+       # выбираем значения из словаря и добавляем в базу
+        for i in s.data:
+            if i is None or i['E_ID'] is None or i['E_NAME'] is None or i['E_DESIGNATION'] is None or i['E_ADDR'] is None or i['E_BRANCH'] is None or i['E_CONT_NO'] is None:
+                logging.error("Error: Some of the values in the CSV file are empty")
+                raise ValueError("Error: Some of the values in the CSV file are empty")
+            
+            # 1. выбираем строку из базы, если есть, то обновляем, если нет, то добавляем
+            self.cur.execute("""
+                SELECT * FROM EMPLOYEE WHERE E_ID = ?
+            """, (i['E_ID'],))
+            
+            # 2. если нет, то добавляем
+            row = self.cur.fetchone()
+            
+            if row is None:
+                logging.info(f"Add employee with id {i['E_ID']}")
+                self.cur.execute("""
+                    INSERT INTO EMPLOYEE (E_ID, E_NAME, E_DESIGNATION, E_ADDR, E_BRANCH, E_CONT_NO) 
+                    VALUES (?, ?, ?, ?, ?, ?)
+                """, (i['E_ID'], i['E_NAME'], i['E_DESIGNATION'], i['E_ADDR'], i['E_BRANCH'], i['E_CONT_NO']))
+                
+            # 3. если есть, то обновляем
+            else:
+                logging.info(f"Update employee with id {i['E_ID']}")
+                self.cur.execute("""
+                    UPDATE EMPLOYEE SET E_NAME = ?, E_DESIGNATION = ?, E_ADDR = ?, E_BRANCH = ?, E_CONT_NO = ?
+                    WHERE E_ID = ?
+                """, (i['E_NAME'], i['E_DESIGNATION'], i['E_ADDR'], i['E_BRANCH'], i['E_CONT_NO'], i['E_ID']))
+                
+            # 4. сохраняем изменения
+            self.conn.commit()
+            
+        logging.info("End adding employee")
+            
+            
+s = SQLmeneg('./Employee_Details.csv')
+s.create_db()
+s.add_employee()
